41dd6691addb2ce6e67aef6a8e474e323944d665
Jessica Estevam <jessicafpereira16@gmail.com>
2024-06-10 18:58:07 -0300

    first day project

---
+++ mock.js
@@ -0,0 +1,32 @@
+const produtos=[
+    {
+    title:"Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+    previous_price:582.90,
+    price:238.41,
+    installment_price:"5x de R$ 47,68 sem juros",
+    img:"src/foto.png"
+    },
+    {
+        title:"Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+        previous_price:582.90,
+        price:238.41,
+        installment_price:"5x de R$ 47,68 sem juros",
+        img:"src/foto (1).png"
+    },
+    {
+    title:"Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+    previous_price:582.90,
+    price:238.41,
+    installment_price:"5x de R$ 47,68 sem juros",
+    img:"src/foto (2).png"
+    },
+    {
+    title:"Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+    previous_price:582.90,
+    price:238.41,
+    installment_price:"5x de R$ 47,68 sem juros",
+    img:"src/foto (3).png"
+    },
+
+
+]
