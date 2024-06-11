99a01934e2ff2f2dbe9defc3aeee5558fea2c4ea
Jessica Estevam <jessicafpereira16@gmail.com>
2024-06-11 15:43:33 -0300

    finalizing application and delivering

--- mock.js
+++ mock.js
@@ -1,32 +1,34 @@
-const produtos=[
-    {
-    title:"Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
-    previous_price:582.90,
-    price:238.41,
-    installment_price:"5x de R$ 47,68 sem juros",
-    img:"src/foto.png"
-    },
-    {
-        title:"Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
-        previous_price:582.90,
-        price:238.41,
-        installment_price:"5x de R$ 47,68 sem juros",
-        img:"src/foto (1).png"
-    },
-    {
-    title:"Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
-    previous_price:582.90,
-    price:238.41,
-    installment_price:"5x de R$ 47,68 sem juros",
-    img:"src/foto (2).png"
-    },
-    {
-    title:"Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
-    previous_price:582.90,
-    price:238.41,
-    installment_price:"5x de R$ 47,68 sem juros",
-    img:"src/foto (3).png"
-    },
-
-
-]
+export const produtos = [
+  {
+    id: 1,
+    title: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+    previous_price: "R$ 582.90",
+    price: "R$ 238.41",
+    installment_price: "5x de R$ 47,68 sem juros",
+    img: "src/foto.png",
+  },
+  {
+    id: 2,
+    title: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+    previous_price: "R$ 582.90",
+    price: "R$ 238.41",
+    installment_price: "5x de R$ 47,68 sem juros",
+    img: "src/foto (1).png",
+  },
+  {
+    id: 3,
+    title: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+    previous_price: "582.90",
+    price: "R$ 238.41",
+    installment_price: "5x de R$ 47,68 sem juros",
+    img: "src/foto (2).png",
+  },
+  {
+    id:4,
+    title: "Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.",
+    previous_price: "R$ 582.90",
+    price: "R$ 238.41",
+    installment_price: "5x de R$ 47,68 sem juros",
+    img: "src/foto (3).png",
+  },
+];
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import {produtos} from './mock.js'
+
 document.addEventListener('DOMContentLoaded', () => {
         const hamburger = document.querySelector('.hamburger');
         const menu = document.querySelector('.menu');
@@ -8,3 +10,60 @@ document.addEventListener('DOMContentLoaded', () => {
 
         
 });
+const ul = document.querySelector(".list_products");
+
+const renderProducts = (products, list) => {
+    console.log(products);
+    products.forEach(element => {
+        const li = document.createElement("li");
+        li.classList.add("card_product")
+        li.innerHTML = `
+            <img src="${element.img}" alt="${element.title}" />
+            <div>
+            <p>${element.title}</p>
+            <span>${element.previous_price}</span>
+            <h4>${element.price}</h4>
+            <h5>${element.installment_price}</h5>
+            </div>
+            <button id=${element.id}>Adicionar ao carrinho</button>
+        `;
+        list.append(li);
+    });
+
+    return list;
+}
+
+renderProducts(produtos, ul);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollContainer = document.querySelector('.list_products');
+    const scrollLeftButton = document.getElementById('scroll-left');
+    const scrollRightButton = document.getElementById('scroll-right');
+
+    const scrollAmount = 275; // Quantidade de pixels a serem rolados por clique
+
+    scrollLeftButton.addEventListener('click', () => {
+        scrollContainer.scrollBy({
+            left: -scrollAmount,
+            behavior: 'smooth'
+        });
+    });
+
+    scrollRightButton.addEventListener('click', () => {
+        scrollContainer.scrollBy({
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+    });
+
+    
+    scrollContainer.addEventListener('scroll', () => {
+        scrollLeftButton.disabled = scrollContainer.scrollLeft === 0;
+        scrollRightButton.disabled = scrollContainer.scrollLeft + scrollContainer.clientWidth >= scrollContainer.scrollWidth;
+    });
+
+ 
+    scrollLeftButton.disabled = scrollContainer.scrollLeft === 0;
+    scrollRightButton.disabled = scrollContainer.scrollLeft + scrollContainer.clientWidth >= scrollContainer.scrollWidth;
+});
+
